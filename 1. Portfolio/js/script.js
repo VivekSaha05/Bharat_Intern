a410398017aef807a8a7a50982710cefff503a7d
vivek <viveksahapop@gmail.com>
2023-06-23 17:04:15 +0530

    Final Commit

--- 1. Portfolio/js/script.js
+++ 1. Portfolio/js/script.js
@@ -54,3 +54,29 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+
+/*--------------Back To top btn--------------*/
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+        window.onscroll = function () {
+            scrollFunction();
+        };
+
+        function scrollFunction() {
+            if (
+                document.body.scrollTop > 20 ||
+                document.documentElement.scrollTop > 20
+            ) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+        mybutton.addEventListener("click", backToTop);
+
+        function backToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
